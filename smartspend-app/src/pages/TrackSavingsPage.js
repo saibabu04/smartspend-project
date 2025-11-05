39c78fdce61b6b1d1ca705295ab14e5044bd56f7
saibabu04 <saibabus625@gmail.com>
2025-11-05 15:00:09 +0530

    Update TrackSavingsPage.js

--- smartspend-app/src/pages/TrackSavingsPage.js
+++ smartspend-app/src/pages/TrackSavingsPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./TrackSavings.css";
 
 const TrackSavingsPage = () => {
