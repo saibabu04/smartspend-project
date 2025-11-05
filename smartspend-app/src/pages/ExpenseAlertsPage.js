a1d6477b4f72880bc42f2303bca03e26ef5ef365
saibabu04 <saibabus625@gmail.com>
2025-11-05 14:57:56 +0530

    Update ExpenseAlertsPage.js
    Added CSS import for styling the Expense Alerts page.

--- smartspend-app/src/pages/ExpenseAlertsPage.js
+++ smartspend-app/src/pages/ExpenseAlertsPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "./ExpenseAlerts.css";
 
 const ExpenseAlertsPage = () => {
   const [alerts, setAlerts] = useState({
